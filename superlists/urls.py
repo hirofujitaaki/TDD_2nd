597cdcb6257667e876c48644321717645492c53d
hiforujitaaki <hirofujitaaki.gmail.com>
2017-06-08 14:04:52 +0900

    new url lists/new added

--- superlists/urls.py
+++ superlists/urls.py
@@ -19,6 +19,9 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^$', views.home_page, name='home'),
+    # no trailing / after 'new' because this is an action URL,
+    # which modify the database.
+    url(r'^lists/new$', views.new_list, name='new_list'),
     url(r'^lists/the-only-list-in-the-world/$', views.view_list,
         name='view_list'),
     # url(r'^admin/', admin.site.urls),
