08db5226c2a1a92c1c8a4a23bdd7f73f121f45b1
hiforujitaaki <hirofujitaaki.gmail.com>
2017-06-09 12:16:46 +0900

    new URL + view for adding to existing lists. FT passes!

--- lists/models.py
+++ lists/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
 
-class Item(models.Model):
+class List(models.Model):
+    pass
 
+
+class Item(models.Model):
     text = models.TextField(default='')
+    list = models.ForeignKey(List, default=None)
+
+
--- superlists/urls.py
+++ superlists/urls.py
@@ -19,10 +19,12 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^$', views.home_page, name='home'),
-    # no trailing / after 'new' because this is an action URL,
-    # which modify the database.
     url(r'^lists/new$', views.new_list, name='new_list'),
-    url(r'^lists/the-only-list-in-the-world/$', views.view_list,
-        name='view_list'),
+    url(r'^lists/(\d+)/$', views.view_list, name='view_list'),
+    url(r'^lists/(\d+)/add_item$', views.add_item, name='add_item'),
     # url(r'^admin/', admin.site.urls),
 ]
+
+
+# no trailing / after 'new' & 'add_item' because this is an action URL,
+# which modify the database.
